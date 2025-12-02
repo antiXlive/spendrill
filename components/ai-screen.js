91cdc1c1d8df2cc48a508463bb7de713ef967cb9
Piyush <piyush107iiitm@gmail.com>
2025-12-02 17:03:47 +0530

    ok

--- components/ai-screen.js
+++ components/ai-screen.js
@@ -5,22 +5,22 @@ class AIScreen extends HTMLElement {
   constructor() {
     super();
     this.images = [
-      "/img/ai.jpg",
-      "/img/ai1.png",
-      "/img/ai2.jpg",
-      "/img/ai3.png",
-      "/img/ai4.jpg",
-      "/img/ai5.jpg",
-      "/img/ai6.jpg",
-      "/img/ai7.jpg",
-      "/img/ai8.jpg",
-      "/img/ai9.jpg",
-      "/img/ai10.jpg",
-      "/img/ai11.jpg",
-      "/img/ai12.jpg",
-      "/img/ai13.jpg",
-      "/img/ai14.jpg",
-      "/img/ai15.jpg",
+      "/spendrill/img/ai.jpg",
+      "/spendrill/img/ai1.png",
+      "/spendrill/img/ai2.jpg",
+      "/spendrill/img/ai3.png",
+      "/spendrill/img/ai4.jpg",
+      "/spendrill/img/ai5.jpg",
+      "/spendrill/img/ai6.jpg",
+      "/spendrill/img/ai7.jpg",
+      "/spendrill/img/ai8.jpg",
+      "/spendrill/img/ai9.jpg",
+      "/spendrill/img/ai10.jpg",
+      "/spendrill/img/ai11.jpg",
+      "/spendrill/img/ai12.jpg",
+      "/spendrill/img/ai13.jpg",
+      "/spendrill/img/ai14.jpg",
+      "/spendrill/img/ai15.jpg",
 
     ];
   }
